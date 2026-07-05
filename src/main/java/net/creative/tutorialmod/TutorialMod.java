9554e8f0f9ebcf65d3a2a3fdc10148beb69d5cc2
tcashj <tanner.jacob@okstate.edu>
2026-07-05 01:33:13 -0500

    Item Creation added

--- src/main/java/net/creative/tutorialmod/TutorialMod.java
+++ src/main/java/net/creative/tutorialmod/TutorialMod.java
@@ -2,17 +2,17 @@ package net.creative.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.resources.Identifier;
-
+import net.creative.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Important Comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/creative/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/creative/tutorialmod/TutorialModDataGenerator.java
@@ -2,10 +2,13 @@ package net.creative.tutorialmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.creative.tutorialmod.datagen.ModModelProvider;
 
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
